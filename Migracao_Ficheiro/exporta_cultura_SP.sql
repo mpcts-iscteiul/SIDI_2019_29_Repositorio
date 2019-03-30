d33da3c24d061aceda4b77404b3f26b4a02cdd60
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-03-30 08:40:10 +0000

    Update exporta_cultura_SP.sql

--- Migracao_Ficheiro/exporta_cultura_SP.sql
+++ Migracao_Ficheiro/exporta_cultura_SP.sql
@@ -4,7 +4,7 @@ if ( (SELECT MIN(id) FROM cultura_log WHERE Flag_Migracao =0) is not null)
 then set @command = 'select DataHora, DescricaoCultura_NEW, DescricaoCultura_OLD, Email_Investigador_NEW, Email_Investigador_OLD, ID, IDCultura_NEW, IDCultura_OLD, NomeCultura_NEW, NomeCultura_OLD, Operacao, Utilizador from cultura_log where ID>=(SELECT MIN(id) FROM cultura_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM cultura_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
-set @conc = concat (@command, ' into outfile "', "C:/xampp/CULTURA_", @command_dois, @nome_ficheiro);
+set @conc = concat (@command, ' into outfile "', "C:/xampp/mysql/data/db_mysql_destino/cultura_log_", @command_dois, @nome_ficheiro);
 
 prepare q from @conc;
 execute q;
