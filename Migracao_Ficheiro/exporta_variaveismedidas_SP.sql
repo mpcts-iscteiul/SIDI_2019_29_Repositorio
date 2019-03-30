e2d4a88ac5a1909e97fbb27924a4ac000059dec8
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-03-30 08:55:29 +0000

    Update exporta_variaveismedidas_SP.sql

--- Migracao_Ficheiro/exporta_variaveismedidas_SP.sql
+++ Migracao_Ficheiro/exporta_variaveismedidas_SP.sql
@@ -4,7 +4,7 @@ if ( (SELECT MIN(id) FROM variaveismedidas_log WHERE Flag_Migracao =0) is not nu
 then set @command = 'select DataHora, ID, IDCultura_NEW, IDCultura_OLD, IDVariavel_Variaveis_NEW, IDVariavel_Variaveis_OLD, LimiteInferior_NEW, LimiteInferior_OLD, LimiteSuperior_NEW, LimiteSuperior_OLD, Operacao, Utilizador from variaveismedidas_log where ID>=(SELECT MIN(id) FROM variaveismedidas_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM variaveismedidas_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
-set @conc = concat (@command, ' into outfile "', "C:/xampp/VARIAVEISMEDIDAS_", @command_dois, @nome_ficheiro);
+set @conc = concat (@command, ' into outfile "', "C:/xampp/mysql/data/db_mysql_destino/variaveismedidas_log_", @command_dois, @nome_ficheiro);
 
 prepare q from @conc;
 execute q;
