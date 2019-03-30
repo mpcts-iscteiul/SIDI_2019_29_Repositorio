9527014d57856095c9e5c0585f1fdeeaa8f12591
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-03-30 08:37:47 +0000

    Update exporta_consultados_SP.sql

--- Migracao_Ficheiro/exporta_consultados_SP.sql
+++ Migracao_Ficheiro/exporta_consultados_SP.sql
@@ -4,7 +4,7 @@ if ( (SELECT MIN(id) FROM medicoes_consultados_log WHERE Flag_Migracao =0) is no
 then set @command = 'select DataHoraMedicao, DataHora_LOG, ID, IDCultura_VariaveisMedidas, IDMedicoes_LOG, IDVariaveis_VariaveisMedidas, NumeroMedicao, Operacao_LOG, Utilizador_LOG, ValorMedicao from medicoes_consultados_log where ID>=(SELECT MIN(id) FROM medicoes_consultados_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM medicoes_consultados_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
-set @conc = concat (@command, ' into outfile "', "C:/xampp/CONSULTADOS_", @command_dois, @nome_ficheiro);
+set @conc = concat (@command, ' into outfile "', "C:/xampp/data/db_mysql_destino/medicoes_consultados_log_", @command_dois, @nome_ficheiro);
 
 prepare q from @conc;
 execute q;
