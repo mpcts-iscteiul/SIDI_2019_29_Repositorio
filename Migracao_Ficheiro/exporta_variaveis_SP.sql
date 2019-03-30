f225c24015a9f7c6a9627b0063fb4d014c0af8b7
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-03-30 08:54:43 +0000

    Update exporta_variaveis_SP.sql

--- Migracao_Ficheiro/exporta_variaveis_SP.sql
+++ Migracao_Ficheiro/exporta_variaveis_SP.sql
@@ -4,7 +4,7 @@ if ( (SELECT MIN(id) FROM variaveis_log WHERE Flag_Migracao =0) is not null)
 then set @command = 'select DataHora, ID, IDVariavel_NEW, IDVariavel_OLD, NomeVariavel_OLD, Operacao, Utilizador) SELECT DataHora, ID, IDVariavel_NEW, IDVariavel_OLD, NomeVariavel_OLD, Operacao, Utilizador from variaveis_log where ID>=(SELECT MIN(id) FROM variaveis_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM variaveis_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
-set @conc = concat (@command, ' into outfile "', "C:/xampp/VARIAVEIS_", @command_dois, @nome_ficheiro);
+set @conc = concat (@command, ' into outfile "', "C:/xampp/mysql/data/db_mysql_destino/variaveis_log_", @command_dois, @nome_ficheiro);
 
 prepare q from @conc;
 execute q;
