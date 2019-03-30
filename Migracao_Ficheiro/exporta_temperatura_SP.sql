de73e3e709bd00f9b97a6fcdf7ca4711691684ac
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-03-30 08:53:50 +0000

    Update exporta_temperatura_SP.sql

--- Migracao_Ficheiro/exporta_temperatura_SP.sql
+++ Migracao_Ficheiro/exporta_temperatura_SP.sql
@@ -4,7 +4,7 @@ if ( (SELECT MIN(id) FROM medicoestemperatura_log WHERE Flag_Migracao =0) is not
 then set @command = 'select DataHoraMedicao, DataHora_LOG, ID, IDCultura_VariaveisMedidas, IDMedicoes_LOG, IDVariaveis_VariaveisMedidas, NumeroMedicao, Operacao_LOG, Utilizador_LOG, ValorMedicao from medicoestemperatura_log where ID>=(SELECT MIN(id) FROM medicoestemperatura_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM medicoestemperatura_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
-set @conc = concat (@command, ' into outfile "', "C:/xampp/TEMPERATURA_", @command_dois, @nome_ficheiro);
+set @conc = concat (@command, ' into outfile "', "C:/xampp/mysql/data/db_mysql_destino/medicoestemperatura_log_", @command_dois, @nome_ficheiro);
 
 prepare q from @conc;
 execute q;
